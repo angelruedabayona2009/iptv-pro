79a27104d201d8f8272e0b08617ac5864d9c3054
angelruedabayona2009 <angelruedabayona2009@gmail.com>
2026-04-20 09:20:01 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import sqlite3
+iimport sqlite3
 
 def get_db():
     return sqlite3.connect("database.db", check_same_thread=False)
@@ -7,21 +7,12 @@ def init_db():
     db = get_db()
 
     db.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
-    )
-    """)
-
-    db.execute("""
-    CREATE TABLE IF NOT EXISTS listas (
+    CREATE TABLE IF NOT EXISTS urls (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         url TEXT UNIQUE,
-        estado TEXT,
-        exp TEXT,
-        canales INTEGER,
-        ultima_revision TEXT
+        status TEXT,
+        response_time REAL,
+        last_check TEXT
     )
     """)
 
